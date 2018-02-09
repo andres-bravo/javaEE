1c0dc5378f30cf59eacdc54ccc822136d1ba943b
manana <manana@Core11-41>
2018-02-09 13:29:56 +0100

    26_agenda_mvc2

---
+++ 26_agenda_mvc2/src/servlets/RecuperarAction.java
@@ -0,0 +1,30 @@
+package servlets;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import beans.Contacto;
+import modelo.GestionContactos;
+
+/**
+ * Servlet implementation class RecuperarAction
+ */
+@WebServlet("/RecuperarAction")
+public class RecuperarAction extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		GestionContactos gcontactos=new GestionContactos();
+		List<Contacto> contactos=gcontactos.recuperarTodos();
+		request.setAttribute("contactos", contactos);
+		request.getRequestDispatcher("contactos.jsp").forward(request, response);
+	}
+
+}
